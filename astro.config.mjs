8bb1443f187f53a705928f3524e29f2c39effcf1
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-16 14:02:06 +0000

    Googleフォント

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
@@ -10,5 +10,19 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ts.dictionary4.dev',
-  integrations: [svelte(), vue(), mdx()]
+  integrations: [svelte(), vue(), mdx()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Fira Code",
+        cssVariable: "--font-fira-code"
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'M PLUS 1 Code',
+        cssVariable: "--font-m-plus-1-code"
+      }
+    ]
+  }
 });
