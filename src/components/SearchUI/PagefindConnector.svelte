0eb2b3b229e1cfa284f222248ca2ea55b505a25d
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-21 07:52:40 +0000

    こんなもんでええか

--- src/components/SearchUI/PagefindConnector.svelte
+++ src/components/SearchUI/PagefindConnector.svelte
@@ -14,7 +14,7 @@
 
   // PagefindUIのオプションに基づいた設定
   const PAGEFIND_OPTIONS = {
-    bundlePath: 'https://js.dictionary4.dev/content_search/',
+    bundlePath: 'https://tsdict.mdmbypass.wing.osaka/content_search/',
     baseUrl: "/content/",
     pageSize: 8,
     // highlightParamは検索結果のハイライト処理にカスタムで使用します
@@ -137,10 +137,10 @@
   <div class="pgSearch">
     <input 
       type="search" 
-      placeholder="サイト内検索..." 
+      placeholder="検索語句を入力" 
       value={query} 
       on:input={handleInput}
-      aria-label="サイト内検索"
+      aria-label="検索語句を入力"
     />
     {#if (query !== '')}
       <button
@@ -159,28 +159,28 @@
       <p class='ステータス'>検索中...</p>
     {:else if query && searchResults.length > 0}
       <p class='ステータス'>{searchResults.length}件の結果を表示</p>
-      <ul class="results-list">
+      <ul class="結果一覧">
         {#each searchResults as result}
           <hr />
-          <li class="result-item">
-            <a href="{result.url}">
-              <h4>{result.meta.title || result.url}</h4>
-              <p class="excerpt">{@html result.excerpt}</p>
+          <li class="項目">
+            <a class='項目リンク' href="{result.url}">
+              <h2>{result.meta.title || result.url}</h2>
+              <p class="詳細">{@html result.excerpt}</p>
             </a>
           </li>
           {#if result.sub_results && result.sub_results.length > 0}
-              <ul class="sub-results-list">
-                {#each result.sub_results as subResult, index}
-                  {#if index > 0} 
-                    <li class="sub-result-item">
-                      <a href="{subResult.url}">
-                        <span class="sub-result-title">{subResult.title}</span>
-                      </a>
-                    </li>
-                  {/if}
-                {/each}
-              </ul>
-            {/if}
+            <ul class="サブ結果">
+              {#each result.sub_results as subResult, index}
+                {#if index >= 0} 
+                  <li class="中身">
+                    <a href="{subResult.url}">
+                      <h3 class="タイトル">{subResult.title}</h3>
+                    </a>
+                  </li>
+                {/if}
+              {/each}
+            </ul>
+          {/if}
         {/each}
         <hr/>
       </ul>
@@ -215,6 +215,42 @@
     .search-output {
       .ステータス {
         text-align: center;
+      }
+      .結果一覧 {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        hr {
+          margin-left: 30px;
+        }
+        .項目 {
+          margin: 0 17px;
+          .項目リンク {
+            h2, p {
+              font-family: var(--font-fira-code), var(--font-zen-kaku-gothic-new), monospace;
+            }
+            h2 {
+              margin-top: 15px;
+              line-height: 1;
+            }
+            .詳細 {
+              margin-top: -10px;
+              color: var(--foreground);
+            }
+          }
+          margin-bottom: -16px;
+        }
+        .サブ結果 {
+          .中身 {
+            a {
+              .タイトル {
+                margin: 0;
+                line-height: 0.8;
+                margin-top: -6px;
+              }
+            }
+          }
+        }
       }
     }
   }
