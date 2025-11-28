fdae9358b52e2ecdc4e522d6d3014d3a43eef1f7
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-28 03:02:48 +0000

    QueryLink

---
+++ src/components/QueryLink.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+  export let href: string;
+
+  import { onMount } from "svelte";
+  import { addQuery } from "./SearchUI/addQuery.mts";
+
+  let query: string = '';
+  let mode: string = '';
+
+  onMount(async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    query = urlParams.get('q') ?? '';
+    mode = urlParams.get('m') ?? '';
+  })
+</script>
+
+<a
+  href={addQuery(href, {
+    'q': query,
+    'm': mode,
+  })}
+>
+  <slot />
+</a>
